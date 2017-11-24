cefc7fd027e497a854cbfb860253ea5048dbf656
KTryCode <32237739+KTryCode@users.noreply.github.com>
2017-11-24 12:29:33 +0100

    0.2

--- app/src/main/java/com/ktrycode/courtcounter/MainActivity.java
+++ app/src/main/java/com/ktrycode/courtcounter/MainActivity.java
@@ -2,12 +2,61 @@ package com.ktrycode.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int scoreTeamA = 0;
+    private int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //Team A methods
+
+    public void threePointsThrowA(View view) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void twoPointsThrowA(View view) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void freeThrowA(View view) {
+        scoreTeamA++;
+        displayForTeamA(scoreTeamA);
+    }
+
+
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    //Team B methods
+
+    public void threePointsThrowB(View view) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void twoPointsThrowB(View view) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void freeThrowB(View view) {
+        scoreTeamB++;
+        displayForTeamB(scoreTeamB);
+    }
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
 }
